b753c44be42182621cf921aecf6c054edc745b7c
Hilol <97358655+Khiloll@users.noreply.github.com>
2023-10-06 12:41:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
- document.getElementById("card").addEventListener("click", function(event) {
-    event.preventDefault(); // Отменяем стандартное поведение перехода по ссылке
-    let pCursor = document.getElementById("cursor");
+function hidePCursor(event) {
+  event.preventDefault(); // Отменяем стандартное поведение перехода по ссылке
+  let pCursor = document.getElementById("cursor");
 
-    pCursor.style.display = "none";
-  });
+  pCursor.style.display = "none";
+}
 
-  document.getElementById("cursor").addEventListener("click", function(event) {
-    event.preventDefault(); // Отменяем стандартное поведение перехода по ссылке
-    let pCursor = document.getElementById("cursor");
-
-    pCursor.style.display = "none";
-  });
-
-  document.getElementById("card-1").addEventListener("click", function(event) {
-    event.preventDefault(); // Отменяем стандартное поведение перехода по ссылке
-    let pCursor = document.getElementById("cursor");
-
-    pCursor.style.display = "none";
-  });
+document.getElementById("card").addEventListener("click", hidePCursor);
+document.getElementById("cursor").addEventListener("click", hidePCursor);
+document.getElementById("card-1").addEventListener("click", hidePCursor);
